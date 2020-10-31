63680bd357fb31209e936a9e81ba0fcf84a568e8
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-31 14:35:07 +0000

    Url adjusts

--- src/Hydra.IdentityServer/Controllers/AccountController.cs
+++ src/Hydra.IdentityServer/Controllers/AccountController.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-
-using EasyNetQ;
 using Hydra.Core.Integration.Messages;
 using Hydra.Core.MessageBus;
 using Hydra.IdentityServer.Helpers;
